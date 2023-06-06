2a764cc82baed5d4aace6545052b46956fe53d3d
Maxim Gorin <maksim.g@simple.app>
2023-06-06 15:47:25 +0300

    First

---
+++ src/lib.rs
@@ -0,0 +1,42 @@
+use std::env;
+
+pub fn get_app_info() -> AppInfo {
+    let app_name = env::var("APP_NAME");
+    let app_name = if let Ok(app_name) = app_name {
+        app_name
+    } else {
+        format!("{}-dev", env!("CARGO_PKG_NAME"))
+    };
+
+    let app_version = env::var("APP_VERSION");
+    let app_version = if let Ok(app_version) = app_version {
+        app_version
+    } else {
+        format!("{}:dev", env!("CARGO_PKG_VERSION"))
+    };
+
+    let app_location = env::var("APP_LOCATION");
+    let app_location = if let Ok(app_location) = app_location {
+        app_location
+    } else {
+        "local".to_string()
+    };
+
+    AppInfo {
+        name: app_name,
+        version: app_version,
+        location: app_location
+    }
+}
+
+pub struct AppInfo {
+    pub name: String,
+    pub version: String,
+    pub location: String,
+}
+
+impl AppInfo {
+    pub fn get_enriched_name(&self) -> String {
+        format!("{}-{}", self.name, self.location)
+    }
+}
---
+++ target/debug/deps/service_sdk-ba4171020f9c7d2c.d
@@ -0,0 +1,8 @@
+D:\Repositories\yourfin-enon\service-sdk\target\debug\deps\service_sdk-ba4171020f9c7d2c.rmeta: src\lib.rs
+
+D:\Repositories\yourfin-enon\service-sdk\target\debug\deps\service_sdk-ba4171020f9c7d2c.d: src\lib.rs
+
+src\lib.rs:
+
+# env-dep:CARGO_PKG_NAME=service-sdk
+# env-dep:CARGO_PKG_VERSION=0.1.0
